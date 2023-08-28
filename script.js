1de76ff60d6b0e5ee7d55f90bc10da756ca315d7
Lost-in-Noise <nancykikvadze@gmail.com>
2023-08-29 00:57:49 +0400

    some

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 'use strict';
 
 // 16.1;
+// let languages = ['html', 'css', 'javascript', 'python', 'php'];
 
-let languages = ['html', 'css', 'javascript', 'python', 'php'];
+// let newArrayLanguages = languages.filter(item => item.length > 3);
 
-let newArrayLanguages = languages.filter(item => item.length > 3);
+// console.log(newArrayLanguages);
 
-console.log(newArrayLanguages);
+// 16.3;
+// let arr = [-1, -2, -3, 4].some((item) => item > 0);
+// console.log(arr);
 
