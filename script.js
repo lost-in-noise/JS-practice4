4370dc0c79070a8196b1a163824f4c6c8e303f70
Lost-in-Noise <nancykikvadze@gmail.com>
2023-08-29 00:53:21 +0400

    filter

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// 16.1;
+
+let languages = ['html', 'css', 'javascript', 'python', 'php'];
+
+let newArrayLanguages = languages.filter(item => item.length > 3);
+
+console.log(newArrayLanguages);
+
